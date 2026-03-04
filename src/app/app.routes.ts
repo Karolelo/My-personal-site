16964bf3a139d669aacc3d3888e180247ea3335c
Karol <kamasahi192@gmail.com>
2026-03-04 10:34:09 +0100

    Refactor app structure: replace placeholder content, add routing, and integrate reusable navbar component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {MainPage} from './pages/main-page/main-page';
+import {ProjectInfo} from './pages/project-info/project-info';
+import {ContactPage} from './pages/contact-page/contact-page';
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: MainPage },
+  { path: 'projects', component: ProjectInfo },
+  { path: 'contact', component: ContactPage },
+  { path: '**', redirectTo: '/home'}
+];
